b91cf0d56dff3710ded9b5251f974338507cd6c9
William Murphy <w.williamjmurphy@gmail.com>
2023-09-07 14:26:01 -0600

    models patly done

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod models;
+mod db;
 
 fn main() {
     println!("Hello, world!");
--- src/models.rs
+++ src/models.rs
@@ -1,28 +1,51 @@
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
 pub enum Status {
-    // TODO: add fields (make sure the fields are public)
+    Open,
+    InProgress,
+    Resolved,
+    Closed
 }
 
 pub struct Epic {
-    // TODO: add fields (make sure the fields are public)
+    name: String,
+    description: String,
+    status: Status,
+    stories: Vec<i32>
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open and the stories should be an empty vector
+        Epic {
+            name,
+            description,
+            status: Status::Open,
+            stories: vec![]
+        }
     }
 }
 
 pub struct Story {
-    // TODO: add fields (make sure the fields are public)
+    name: String,
+    description: String,
+    status: Status
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        todo!() // by default the status should be set to open
+        Story {
+            name,
+            description,
+            status: Status::Open,
+        }
     }
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct DBState {
     // This struct represents the entire db state which includes the last_item_id, epics, and stories
-    // TODO: add fields (make sure the fields are public)
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>
 }
