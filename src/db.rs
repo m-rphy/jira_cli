96e940c39c52aba11218c57b92713ad5fd28e642
William Murphy <w.williamjmurphy@gmail.com>
2023-09-07 17:54:20 -0600

    adding db impls

--- src/db.rs
+++ src/db.rs
@@ -3,6 +3,58 @@ use anyhow::{Result, Context};
 
 use crate::models::{DBState, Epic, Story, Status};
 
+pub struct JiraDatabase {
+    database: Box<dyn Database>
+}
+
+impl JiraDatabase {
+    pub new(file_path: String) -> Self {
+        Self {
+            database: Box::new(JSONFileDatabase { file_path })
+        }
+    }
+
+    pub fn read_db(&self) -> Result<DBState> {
+        self.database.read_db()
+    }
+
+    pub fn create_epic(&self, epic: Epic) -> Result<u32> {
+        let mut parsed = self.database.read_db()?;
+
+        let last_id = parse.last_item_ide;
+        let new_id = last_id + 1;
+
+        parsed.last_item_id = new_id;
+        parsed.epics.insert(new_id, epic)
+            .with_context(|| format!("Failed to insert epic: {}", &epic))?;
+
+        self.database.write_db(&parsed)
+            .with_context(|| format!("Failed to write {} to database", &epic))?;
+                
+            
+        Ok(new_id)
+    }
+
+    pub fn create_story(&self, story: Story, epic_id: u32) -> Result<u32> {
+        let mut parsed = self.database.read_db()?;
+
+        let last_id = parse.last_item_ide;
+        let new_id = last_id + 1;
+
+        parsed.last_item_id = new_id;
+        parsed.stories.insert(new_id, story)
+            .with_context(|| format!("Failed to insert story: {}", &story))?;
+
+        parsed.epics.get_mut(&epic_id).ok_or_else(|| anyhow!("coundl not find epic in database!"))?.storuies.push(new_id);
+
+        self.database.write_db(&parsed)
+            .with_context(|| format!("Failed to write {} to database", &epic))?;
+                
+            
+        Ok(new_id)
+    }
+}
+
 trait Database {
     fn read_db(&self) -> Result<DBState>;
     fn write_db(&self, db_state: &DBState) -> Result<()>;
@@ -32,6 +84,82 @@ impl Database for JSONFileDatabase {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::test_utils::MockDB;
+
+    #[test]
+    fn create_epic_should_work() {
+        let db = JiraDatabase { database: Box::new(MockDB::new()) };
+        let epic = Epic::new("".to_owned(), "".to_owned());
+
+        // TODO: fix this error by deriving the appropriate traits for Epic
+        let result = db.create_epic(epic.clone());
+        
+        assert_eq!(result.is_ok(), true);
+
+        let id = result.unwrap();
+        let db_state = db.read_db().unwrap();
+
+        let expected_id = 1;
+
+        assert_eq!(id, expected_id);
+        assert_eq!(db_state.last_item_id, expected_id);
+        assert_eq!(db_state.epics.get(&id), Some(&epic));
+    }
+
+    #[test]
+    fn create_story_should_error_if_invalid_epic_id() {
+        let db = JiraDatabase { database: Box::new(MockDB::new()) };
+        let story = Story::new("".to_owned(), "".to_owned());
+
+        let non_existent_epic_id = 999;
+
+        let result = db.create_story(story, non_existent_epic_id);
+        assert_eq!(result.is_err(), true);
+    }
+
+    #[test]
+    fn create_story_should_work() {
+        let db = JiraDatabase { database: Box::new(MockDB::new()) };
+        let epic = Epic::new("".to_owned(), "".to_owned());
+        let story = Story::new("".to_owned(), "".to_owned());
+
+        let result = db.create_epic(epic);
+        assert_eq!(result.is_ok(), true);
+
+        let epic_id = result.unwrap();
+
+        // TODO: fix this error by deriving the appropriate traits for Story
+        let result = db.create_story(story.clone(), epic_id);
+        assert_eq!(result.is_ok(), true);
+
+        let id = result.unwrap();
+        let db_state = db.read_db().unwrap();
+
+        let expected_id = 2;
+
+        assert_eq!(id, expected_id);
+        assert_eq!(db_state.last_item_id, expected_id);
+        assert_eq!(db_state.epics.get(&epic_id).unwrap().stories.contains(&id), true);
+        assert_eq!(db_state.stories.get(&id), Some(&story));
+    }
+
+    #[test]
+    fn update_story_status_should_work() {
+        let db = JiraDatabase { database: Box::new(MockDB::new()) };
+        let epic = Epic::new("".to_owned(), "".to_owned());
+        let story = Story::new("".to_owned(), "".to_owned());
+
+        let result = db.create_epic(epic);
+        let epic_id = result.unwrap();
+        let result = db.create_story(story, epic_id);
+        let story_id = result.unwrap();
+        let result = db.update_story_status(story_id, Status::Closed);
+        assert_eq!(result.is_ok(), true);
+        let db_state = db.read_db().unwrap();
+
+        assert_eq!(db_state.stories.get(&story_id).unwrap().status, Status::Closed);
+    }
+
 
     mod database {
         use std::collections::HashMap;
